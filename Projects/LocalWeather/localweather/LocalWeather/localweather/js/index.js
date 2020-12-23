1e3450e47a4eb3e9d95bb9a67d9306f03d3dae39
MaciekB <maciekbajda92@hotmail.com>
2020-12-23 23:47:53 +0000

    uncommeneted  https  config

--- Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
+++ Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
@@ -35,7 +35,7 @@ $(document).ready(function ()
             //       reset:true
             //    });
             
-            // L.mapbox.config.FORCE_HTTPS = true;
+            L.FORCE_HTTPS = true;
                   L.tileLayer('https://api.mapbox.com/styles/v1/mbajda/ckj1thqqu0r7t19p30ui4b1vn/wmts?access_token=pk.eyJ1IjoibWJhamRhIiwiYSI6ImNrajF0ZTA2czJ5ajEydm1tcnZjYnhuczQifQ.wt5bVKziFjaIRXlOREJH7Q', {
                      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                      maxZoom: 13,
