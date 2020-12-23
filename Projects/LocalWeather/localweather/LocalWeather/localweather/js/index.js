5bf9c2ececcf644572f9e90748320c76c9133f2e
MaciekB <maciekbajda92@hotmail.com>
2020-12-23 22:43:18 +0000

    changed zoom lever  from 18 to 13

--- Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
+++ Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
@@ -38,7 +38,7 @@ $(document).ready(function ()
             // L.mapbox.config.FORCE_HTTPS = true;
                   L.tileLayer('https://api.mapbox.com/styles/v1/mbajda/ckj1thqqu0r7t19p30ui4b1vn/wmts?access_token=pk.eyJ1IjoibWJhamRhIiwiYSI6ImNrajF0ZTA2czJ5ajEydm1tcnZjYnhuczQifQ.wt5bVKziFjaIRXlOREJH7Q', {
                      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                     maxZoom: 18,
+                     maxZoom: 13,
                      id: 'mapbox/styles/mbajda/v1',
                      tileSize: 512,
                      zoomOffset: -1,
