1393f91c3c33d376896a3f8a950fade8d6a3cedc
MaciekB <maciekbajda92@hotmail.com>
2020-12-24 00:04:47 +0000

    changed the url to mapbox-settelite 9

--- Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
+++ Projects/LocalWeather/localweather/LocalWeather/localweather/js/index.js
@@ -36,7 +36,7 @@ $(document).ready(function ()
             //    });
             
             L.FORCE_HTTPS = true;
-                  L.tileLayer('https://api.mapbox.com/styles/v1/mbajda/ckj1thqqu0r7t19p30ui4b1vn/wmts?access_token=pk.eyJ1IjoibWJhamRhIiwiYSI6ImNrajF0ZTA2czJ5ajEydm1tcnZjYnhuczQifQ.wt5bVKziFjaIRXlOREJH7Q', {
+                  L.tileLayer('https://api.mapbox.com/mapbox/satellite-v9?access_token=pk.eyJ1IjoibWJhamRhIiwiYSI6ImNrajF0ZTA2czJ5ajEydm1tcnZjYnhuczQifQ.wt5bVKziFjaIRXlOREJH7Q', {
                      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                      maxZoom: 13,
                      id: 'mapbox.satellite',
