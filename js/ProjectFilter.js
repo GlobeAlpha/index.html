a90cf4dc22b91aabc27dc4b520a2302f8797ef4d
MaciekB <maciekbajda92@hotmail.com>
2021-01-12 20:15:40 +0000

    Created Project Filter

---
+++ js/ProjectFilter.js
@@ -0,0 +1,16 @@
+//access value of radio  button
+var ProjectCommercial = document.getElementById('Commercial').value;
+var ProjectPersonal = document.getElementById('Personal').value;
+//access value of select  
+var Vanilla_Option = document.getElementById("HTML/CSS").value
+var JS_Option = document.getElementById("JS").value
+var React_Option  = document.getElementById("React").value
+
+// log the value of radion  button and select
+
+console.log(ProjectCommercial);
+console.log(ProjectPersonal);
+
+console.log(Vanilla_Option);
+console.log(JS_Option);
+console.log(React_Option);
