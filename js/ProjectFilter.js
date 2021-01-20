576473454a5c3fd068ae80f3381ad927e3c91377
MaciekB <maciekbajda92@hotmail.com>
2021-01-20 06:43:51 +0000

    Created a Project Gallery

--- js/ProjectFilter.js
+++ js/ProjectFilter.js
@@ -7,71 +7,80 @@ var ProjectPersonal = document.getElementById('Personal');
 
 
 
-Project_Type = () =>
-{
-    if (ProjectCommercial.checked == true)
-    {
+// Project_Type = () =>
+// {
+//     if (ProjectCommercial.checked == true)
+//     {
 
-        if (ProjectCommercial.value = "Commercial")
-        {
-            var Personal = document.getElementById("PersonalCunt")
-            var Commercial =document.getElementById("CommercialCunt")
+//         if (ProjectCommercial.value = "Commercial")
+//         {
+//             var Personal = document.getElementById("PersonalCunt")
+//             var Commercial =document.getElementById("CommercialCunt")
            
-            Personal.style.display = "none"
-            Commercial.style.display = "block"
+//             Personal.style.display = "none"
+//             Commercial.style.display = "block"
         
-        }
-    }
+//         }
+//     }
 
-     else if (ProjectPersonal.checked == true)
-    {
-       if (ProjectPersonal.value = "Personal")
-       {
-        var Personal = document.getElementById("CommercialCunt")
-        var Commercial =document.getElementById("PersonalCunt")
+//      else if (ProjectPersonal.checked == true)
+//     {
+//        if (ProjectPersonal.value = "Personal")
+//        {
+//         var Personal = document.getElementById("CommercialCunt")
+//         var Commercial =document.getElementById("PersonalCunt")
        
-        Personal.style.display = "none"
-        Commercial.style.display = "block"
+//         Personal.style.display = "none"
+//         Commercial.style.display = "block"
       
-            // console.log(Commerrcial)
-        }
-    }
+//             // console.log(Commerrcial)
+//         }
+//     }
   
     
-}
+// }
 
-TechType = () =>
-{
+// TechType = () =>
+// {
 
-    var Tech_Type_Dropdown = document.getElementById("Tech_Types")
-    console.log(Tech_Type_Dropdown)
-    //  acesses  both selected Index 
-    var Options = Tech_Type_Dropdown.options[Tech_Type_Dropdown.selectedIndex].text
+//     var Tech_Type_Dropdown = document.getElementById("Tech_Types")
+//     console.log(Tech_Type_Dropdown)
+//     //  acesses  both selected Index 
+//     var Options = Tech_Type_Dropdown.options[Tech_Type_Dropdown.selectedIndex].text
     
-    var Vanilla_Option = document.getElementById("HTML/CSS").value
-    var JS_Option = document.getElementById("JS").value
-    var React_Option  = document.getElementById("React").value
+//     var Vanilla_Option = document.getElementById("HTML/CSS").value
+//     var JS_Option = document.getElementById("JS").value
+//     var React_Option  = document.getElementById("React").value
     
     
-    // var  Tech_Types = document.getElementsByClassName()
+//     // var  Tech_Types = document.getElementsByClassName()
 
-    if (Options == JS_Option)
-    {
-        console.log(JS_Option)
-        // console.log(`${JS_Option} option has been selected`)
-        }
+//     if (Options == JS_Option)
+//     {
+//         console.log(JS_Option)
+//         var Tech_Tag = document.getElementById("tech_tag");
+//         TechTag = Tech_Tag.style.background = "blue"
+//         Tech_Tag.textContent ="JS"
 
-        else  if (Options == React_Option)
-    {
-        console.log(React_Option)
-            // console.log(`${React_Option} option has been selected`)
-    }
+//         // console.log(`${JS_Option} option has been selected`)
+//         }
+
+//         else  if (Options == React_Option)
+//     {
+//         console.log(React_Option)
+//         var Tech_Tag = document.getElementById("tech_tag");
+//         TechTag = Tech_Tag.style.background ="yellow"
+//             // console.log(`${React_Option} option has been selected`)
+//     }
         
-    else
-    {
-        console.log(`${Vanilla_Option} option has been selected`)
-        }
+//     else
+//     {
+//         var Tech_Tag = document.getElementById("tech_tag");
+//         TechTag = Tech_Tag.style.background = "navajowhite";
+//         console.log(`${Vanilla_Option} option has been selected`)
+//         }
 
- /// write logic that switches to   projects based on tech type 
-}
+//  ///  display tech types based on  tech tags 
+//     //  place an invisible element which has the tech type 
+// }
 
